71b69ae114cbd193e6f82372bfbd2b0d03535376
Trevor Reimer <trevorreimer95@gmail.com>
2019-06-27 19:57:53 -0300

    init

---
+++ lib/index.ts
@@ -0,0 +1,12 @@
+
+
+/**
+ * Register the default route with express
+ * 
+ * @param app Express app
+ * @param socket the websocket connection
+ * @param route the default route
+ */
+export function register(app : HTMLOutputElement, socket : WebSocket, route : String){
+
+}
---
+++ out/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Register the default route with express
+ *
+ * @param app Express app
+ * @param socket the websocket connection
+ * @param route the default route
+ */
+function register(app, socket, route) {
+}
+exports.register = register;
+//# sourceMappingURL=index.js.map
