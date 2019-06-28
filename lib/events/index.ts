741893d2d5714fbf475c007eff5f214e408f9d3a
Trevor Reimer <trevorreimer95@gmail.com>
2019-06-27 21:07:49 -0300

    register the main listeners

---
+++ lib/events/index.ts
@@ -0,0 +1,9 @@
+import {Events} from "./event";
+
+export const get = new Events();
+
+export const post = new Events();
+
+export const put = new Events();
+
+export const del = new Events();
--- out/events/index.js
+++ out/events/index.js
@@ -1,2 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var event_1 = require("./event");
+exports.get = new event_1.Events();
+exports.post = new event_1.Events();
+exports.put = new event_1.Events();
+exports.del = new event_1.Events();
 //# sourceMappingURL=index.js.map
